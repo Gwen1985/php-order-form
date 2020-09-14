2448fa9ccdbce37deca91f2587e2b498db3c2cc2
gwen1985 <gwen.ellegeest@outlook.com>
2020-09-15 00:27:58 +0200

    added comments // code is not fixed!!

--- form-view.php
+++ form-view.php
@@ -31,6 +31,7 @@
             <div></div>
         </div>
 
+
         <fieldset>
             <legend>Address</legend>
 
@@ -66,6 +67,7 @@
             <?php endforeach; ?>
         </fieldset>
 
+
         <button type="submit" class="btn btn-primary">Order!</button>
     </form>
 
@@ -73,12 +75,12 @@
 
     //-----------------------------FORM-VALIDATION----------------------------------------------------------------------
     //set variables
-    $nameFirst = $nameLast = $email = "";
+    //    $nameFirst = $nameLast = $email = "";
 
-    $addressStreet = (isset($_SESSION["address-street"])) ? $_SESSION['address-street'] : '';
-    $addressNumber = (isset($_SESSION["address-number"])) ? $_SESSION['address-number'] : '';
-    $addressZip = (isset($_SESSION["address-zip"])) ? $_SESSION['address-zip'] : '';
-    $addressCity = (isset($_SESSION["address-city"])) ? $_SESSION['address-city'] : '';
+    $street = (isset($_SESSION["street"])) ? $_SESSION['street'] : '';
+    $streetNr = (isset($_SESSION["streetNr"])) ? $_SESSION['streetNr'] : '';
+    $zipcode = (isset($_SESSION["zipcode"])) ? $_SESSION['zipcode'] : '';
+    $city = (isset($_SESSION["city"])) ? $_SESSION['city'] : '';
 
     $nameFirstError = $nameLastError = $emailError = $addressStreetError = $addressNumberError = $addressZipError = $addressCityError = $productError = "";
 
@@ -104,58 +106,51 @@
     }
     //---------------------------------------------STREET---------------------------------------------------------------
 
-    if (!empty($_POST['address-street'])) {
-        $addressStreet = sanitizeData($_POST['address-street']);
+    if (!empty($_POST['street'])) {
+        $street = sanitizeData($_POST['street']);
     } else {
         $isFormValid = false;
         $addressStreetError = $errorPrefix . $errorRequiredText . $errorSuffix;
     }
     //----------------------------------------------STREETNR------------------------------------------------------------
 
-    if (!empty($_POST['address-number'])) {
-        $addressNumber = sanitizeData($_POST['address-number']);
+    if (!empty($_POST['streetNr'])) {
+        $streetNr = sanitizeData($_POST['streetNr']);
     } else {
         $isFormValid = false;
         $addressNumberError = $errorPrefix . $errorRequiredText . $errorSuffix;
     }
     //----------------------------------------------CITY----------------------------------------------------------------
 
-    if (!empty($_POST['address-city'])) {
-        $addressCity = sanitizeData($_POST['address-city']);
+    if (!empty($_POST['city'])) {
+        $city = sanitizeData($_POST['city']);
     } else {
         $isFormValid = false;
         $addressCityError = $errorPrefix . $errorRequiredText . $errorSuffix;
     }
     //-----------------------------------------------ZIPCODE------------------------------------------------------------
 
-    if (!empty($_POST['address-zip'])) {
-        $addressZip = sanitizeData($_POST['address-zip']);
+    if (!empty($_POST['zipcode'])) {
+        $zipcode = sanitizeData($_POST['zipcode']);
     } else {
         $isFormValid = false;
         $addressZipError = $errorPrefix . $errorRequiredText . $errorSuffix;
 
-        //--------------------------------------PRODUCTS--------------------------------------------------------------------
+        //--------------------------------------PRODUCTS----------------------------------------------------------------
 
-        if (!empty($_POST['product'])) {
-            $orderedProducts = $_POST['product'];
+        if (!empty($_POST['foodSelected'])) {
+            $orderedProducts = $_POST['foodselected'];
         } else {
             $isFormValid = false;
             $productError = $errorPrefix . $errorRequiredProductText . $errorSuffix;
         }
 
         if ($isFormValid) {
-//            echo 'Name first: ' . $nameFirst . '<br />';
-//            echo 'Name last: ' . $nameLast . '<br />';
-//            echo 'Email: ' . $email . '<br />';
-//            echo 'Address street: ' . $addressStreet . '<br />';
-//            echo 'Address number: ' . $addressNumber . '<br />';
-//            echo 'Address zip: ' . $addressZip . '<br />';
-//            echo 'Address city: ' . $addressCity . '<br />';
             $orderAmount = calculateOrderAmount($products, $orderedProducts);
             //echo 'Order amount: ' . $orderAmount . '<br />';
             storeOrderAmount($orderAmount);
 
-            //------------------------------------------------------------------------------------------------------------------
+            //----------------------------------------------------------------------------------------------------------
             // ADD FIELDS TO SESSION
             $_SESSION["address-street"] = $addressStreet;
             $_SESSION["address-number"] = $addressNumber;
@@ -210,6 +205,7 @@
         $data = htmlspecialchars($data);
         return $data;
     }
+
     //-----------------------------------------GET--SERVER--INFO--------------------------------------------------------
 
     //get server info
@@ -225,7 +221,7 @@
     //
     //    print_r($server);
     //-----------------------------------------------------------------------------
-    require('product.php');
+    require('index.php');
     ?>
 
     <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
--- index.php
+++ index.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 //we are going to use session variables so we need to enable sessions
 session_start();
 
-function whatIsHappening() {
+function whatIsHappening()
+{
     echo '<h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
@@ -16,8 +17,10 @@ function whatIsHappening() {
     var_dump($_SESSION);
 }
 
-//your products with their price.
-$products = [
+whatIsHappening();
+
+//  FOOD PRODUCTS
+$productsFoods = [
     ['name' => 'Club Ham', 'price' => 3.20],
     ['name' => 'Club Cheese', 'price' => 3],
     ['name' => 'Club Cheese & Ham', 'price' => 4],
@@ -25,12 +28,22 @@ $products = [
     ['name' => 'Club Salmon', 'price' => 5]
 ];
 
-$products = [
+//  DRINKS PRODUCTS
+$productsDrinks = [
     ['name' => 'Cola', 'price' => 2],
     ['name' => 'Fanta', 'price' => 2],
     ['name' => 'Sprite', 'price' => 2],
     ['name' => 'Ice-tea', 'price' => 3],
 ];
+
+// SWITCH BETWEEN FOOD AND DRINKS
+$foodSelected = $_GET['food'];
+if ($foodSelected) {
+    $products = $productsFoods;
+} else {
+    $products = $productsDrinks;
+}
+
 
 $totalValue = 0;
 
